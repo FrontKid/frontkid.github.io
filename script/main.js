08c9edaea39d0a123ed3f02c62fa3a409b113368
Denys Poltorak <itwfrontend@gmail.com>
2022-12-17 14:33:08 +0200

    popup was added

--- script/main.js
+++ script/main.js
@@ -1,8 +1,9 @@
 "use strict";
 import portfolioRenderViaClick from "./components/Portfolio.js";
+import modal from "./components/modal.js"
 window.addEventListener('DOMContentLoaded', () => {
   portfolioRenderViaClick()
-
+  modal()
 })
 
 
